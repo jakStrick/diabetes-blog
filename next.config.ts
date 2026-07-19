4d7be62d68290250749d4df7e2ddd20f061db894
David Strickland <you@youremail.com>
2026-07-18 22:41:03 -0700

    added cloudflare turnstile.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
-  /* config options here, .dev.vars picked up on server start (touched to reload after adding Resend vars) */
+  /* config options here, .dev.vars picked up on server start (touched to reload after adding Turnstile vars) */
 };
 
 export default nextConfig;
